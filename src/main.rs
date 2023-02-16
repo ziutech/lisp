63a0bfe6b5212f4144c15771ccc695e8c5c4a614
Krzysztof Zduniak <krzysz.zdun@gmail.com>
2023-02-16 13:57:57 +0100

    tests

--- src/main.rs
+++ src/main.rs
@@ -39,3 +39,49 @@ fn main() -> Result<(), Box<dyn Error>> {
     _loop(&mut bufin, &mut bufout)?;
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use std::env;
+    use std::fs::File;
+    use std::io::{BufReader, Cursor, Read};
+
+    use crate::rep;
+    use std::iter::zip;
+    fn parse_test(str: &String) -> (String, String) {
+        let mut input = String::new();
+        let mut output = String::new();
+        for line in str.lines() {
+            match line.get(0..=0) {
+                Some(";") => match line.get(1..=1) {
+                    Some(";") => continue,
+                    Some(">") => continue,
+                    Some("=") => {
+                        output.push_str(line.strip_prefix(";=>").unwrap());
+                        output.push_str("\n");
+                    }
+                    Some(_) => continue,
+                    None => continue,
+                },
+                Some(_) => {
+                    input.push_str(line);
+                    input.push_str("\n");
+                }
+                None => continue,
+            }
+        }
+        (input, output)
+    }
+
+    #[test]
+    fn from_file() {
+        let dir = env::var("CARGO_MANIFEST_DIR").unwrap() + "/tests/step0_repl.mal";
+        let mut file = File::open(dir).unwrap();
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
+        let (input_str, expected_output_str) = parse_test(&contents);
+        for (i, exp) in zip(input_str.lines(), expected_output_str.lines()) {
+            assert_eq!(exp, rep(i));
+        }
+    }
+}
